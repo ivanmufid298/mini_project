34e75c30bf3eb1ef62b46bf9560122a89d1a576a
ivan mufid <ivanmohammad2355@gmail.com>
2022-05-19 11:04:29 +0700

    main js fix

--- src/main.js
+++ src/main.js
@@ -3,11 +3,51 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.css";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "apollo-boost";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import VueApollo from "vue-apollo";
 
 Vue.config.productionTip = false;
+
+const httpLink = new HttpLink({
+  uri: "https://light-elephant-62.hasura.app/v1/graphql",
+});
+
+const wsLink = new WebSocketLink({
+  uri: "ws://light-elephant-62.hasura.app/v1/graphql",
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind == "OperationDefinition" &&
+      definition.operation == "subsciption"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
+Vue.use(VueApollo);
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount("#app");
